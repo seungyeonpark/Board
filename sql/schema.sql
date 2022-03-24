a9c4f4066009145ad60cc0857807162dd33952a1
seungyeonpark <siaayeon@gmail.com>
2022-03-24 22:05:33 +0900

    chore: database config

---
+++ sql/schema.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS Board;
+
+CREATE TABLE Board (
+	board_no INT NOT NULL AUTO_INCREMENT,
+	title VARCHAR(200) NOT NULL,
+	content TEXT NULL,
+	writer VARCHAR(50) NOT NULL,
+	reg_date TIMESTAMP NOT NULL DEFAULT now(),
+	PRIMARY KEY(board_no)
+);
--- src/main/java/com/project/board/BoardApplication.java
+++ src/main/java/com/project/board/BoardApplication.java
@@ -1,9 +1,11 @@
 package com.project.board;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "com.project.board.mapper")
 public class BoardApplication {
 
 	public static void main(String[] args) {
