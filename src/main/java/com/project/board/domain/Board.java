bf2279bfc6d9a8dbd7efc81caf95d816704bdce8
seungyeonpark <siaayeon@gmail.com>
2022-03-24 22:08:55 +0900

    feat: entity

---
+++ src/main/java/com/project/board/domain/Board.java
@@ -0,0 +1,28 @@
+package com.project.board.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+public class Board {
+
+    private Long boardNo;
+
+    @NotBlank
+    private String title;
+
+    @NotBlank
+    private String writer;
+
+    private String content;
+
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private LocalDateTime regDate;
+}
