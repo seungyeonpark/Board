6f25ad83dc35ecac4bf1bf08a7bc81fdf1cabf3b
seungyeonpark <siaayeon@gmail.com>
2022-03-25 11:10:14 +0900

    feat: Add bean validation check board-form

--- src/main/java/com/project/board/controller/BoardController.java
+++ src/main/java/com/project/board/controller/BoardController.java
@@ -5,6 +5,8 @@ import com.project.board.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,7 +19,7 @@ public class BoardController {
 
     private final BoardService service;
 
-    @GetMapping(value = {"/", "/list"})
+    @GetMapping("/list")
     public String list(Model model) throws Exception {
 
         model.addAttribute("list", service.list());
@@ -32,7 +34,11 @@ public class BoardController {
     }
 
     @PostMapping("/register")
-    public String register(Board board) throws Exception {
+    public String register(@Validated Board board, BindingResult bindingResult) throws Exception {
+
+        if (bindingResult.hasErrors()) {
+            return "write";
+        }
 
         service.register(board);
         return "redirect:/board/list";
@@ -53,7 +59,11 @@ public class BoardController {
     }
 
     @PostMapping("/modify/{boardNo}")
-    public String modify(@PathVariable Long boardNo, Board board) throws Exception {
+    public String modify(@PathVariable Long boardNo, @Validated Board board, BindingResult bindingResult) throws Exception {
+
+        if (bindingResult.hasErrors()) {
+            return "edit";
+        }
 
         board.setBoardNo(boardNo);
         service.modify(board);
