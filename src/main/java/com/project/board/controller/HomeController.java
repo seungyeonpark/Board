52dbcfcd1eac754110ed16d202c5b783a35e7c07
seungyeonpark <siaayeon@gmail.com>
2022-03-26 19:41:48 +0900

    feat: Add login home

--- src/main/java/com/project/board/controller/HomeController.java
+++ src/main/java/com/project/board/controller/HomeController.java
@@ -1,13 +1,23 @@
 package com.project.board.controller;
 
+import com.project.board.domain.Member;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String home() {
-        return "home";
+    public String home(@SessionAttribute(name = "loginMember", required = false) Member loginMember, Model model) {
+
+        if (loginMember == null) {
+            return "home";
+        }
+
+        model.addAttribute("member", loginMember);
+
+        return "loginHome";
     }
 }
